617b43fddc65f7e0e9575d38e1f8eb97c1c303ed
Den <dabramov@starlabstudio.com>
2019-11-10 22:46:10 +0400

    Minor fixes

--- lib/veider.dart
+++ lib/veider.dart
@@ -3,3 +3,4 @@ library veider;
 export 'package:veider/src/di_container.dart';
 export 'package:veider/src/di_module.dart';
 export 'package:veider/resolvers/resolvers.dart';
+export 'package:veider/src/utils/disposable.dart';
